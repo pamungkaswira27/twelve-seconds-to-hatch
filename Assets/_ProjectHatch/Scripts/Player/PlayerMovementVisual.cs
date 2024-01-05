fbdb7eb183f9a53454a75116fa63f3cffe275ad0
StinkySteak <slimesteve17@gmail.com>
2024-01-05 22:16:13 +0700

    feat: play jump sfx

--- Assets/_ProjectHatch/Scripts/Player/PlayerMovementVisual.cs
+++ Assets/_ProjectHatch/Scripts/Player/PlayerMovementVisual.cs
@@ -1,3 +1,4 @@
+using JSAM;
 using UnityEngine;
 
 namespace ProjectHatch.Player.Movement.Visual
@@ -7,11 +8,18 @@ namespace ProjectHatch.Player.Movement.Visual
         [SerializeField] private PlayerMovement _playerMovement;
         [SerializeField] private GameObject _vfxDustPrefab;
         [SerializeField] private Transform _positionVfxDust;
+        [SerializeField] private SoundFileObject _jumpSfx;
 
         private void Start()
         {
             _playerMovement.OnPlayerJustGrounded += PlayDustVFX;
-            _playerMovement.OnPlayerJumped += PlayDustVFX;
+            _playerMovement.OnPlayerJumped += OnPlayerJumped;
+        }
+
+        private void OnPlayerJumped()
+        {
+            PlayDustVFX();
+            AudioManager.PlaySound(_jumpSfx);
         }
 
         private void PlayDustVFX()
