6fbad2c0cb54201250b9eba6a54cf445518d9a82
StinkySteak <slimesteve17@gmail.com>
2024-01-05 22:14:58 +0700

    sfx: generate jsam sfx

---
+++ Assets/_ProjectHatch/JSAM/AudioEnums - Library.cs
@@ -0,0 +1,12 @@
+namespace ProjectHatch.Audio {
+    public enum Sound {
+        Dash,
+        Dead,
+        Jump,
+        Victory
+    }
+}
+namespace ProjectHatch.Audio {
+    public enum Music {
+    }
+}
