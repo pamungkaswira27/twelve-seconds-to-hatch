c9bcad0324c82096094a79df6420de8242e25a49
StinkySteak <slimesteve17@gmail.com>
2024-01-06 22:30:49 +0700

    feat: play sfx on button click & small refactor

--- Assets/_ProjectHatch/Scripts/UI/GUI/GUIMenu.cs
+++ Assets/_ProjectHatch/Scripts/UI/GUI/GUIMenu.cs
@@ -1,19 +1,30 @@
+using JSAM;
 using ProjectHatch.UI.GUI.Game.Transition;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace ProjectHatch.UI.GUI.Game.Menu
 {
     public class GUIMenu : MonoBehaviour
     {
-        public void PlayGame()
+        [SerializeField] private Button _buttonPlay;
+        [SerializeField] private SoundFileObject _sfxUIButtonClick;
+
+        private void Start()
+        {
+            _buttonPlay.onClick.AddListener(PlayGame);
+        }
+
+        private void PlayGame()
         {
             StartCoroutine(PlayGame_Coroutine());
         }
 
         private IEnumerator PlayGame_Coroutine()
         {
+            AudioManager.PlaySound(_sfxUIButtonClick);
             GUITransition.Instance.PlayEnterTransition();
             yield return new WaitForSeconds(GUITransition.Instance.EnterDuration);
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
