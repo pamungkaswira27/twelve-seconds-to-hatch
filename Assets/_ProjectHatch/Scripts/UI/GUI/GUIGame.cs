e3f4f11c0a40749843d76004fb41357e82384876
StinkySteak <slimesteve17@gmail.com>
2024-01-06 08:52:21 +0700

    feat: guiGame

---
+++ Assets/_ProjectHatch/Scripts/UI/GUI/GUIGame.cs
@@ -0,0 +1,10 @@
+using TMPro;
+using UnityEngine;
+
+namespace ProjectHatch.UI.GUI.Game
+{
+    public class GUIGame : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _textTimer;
+    }
+}
