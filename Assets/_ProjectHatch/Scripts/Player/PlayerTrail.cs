9807a9db13e8505aa7b26d8fb90759c547909d72
StinkySteak <slimesteve17@gmail.com>
2024-01-07 15:14:29 +0700

    vfx: playerTrail now uses custom shader

--- Assets/_ProjectHatch/Scripts/Player/PlayerTrail.cs
+++ Assets/_ProjectHatch/Scripts/Player/PlayerTrail.cs
@@ -7,15 +7,24 @@ namespace ProjectHatch.Player.Movement.Visual
         [SerializeField] private Color _initialColor;
         [SerializeField] private Color _targetColor;
         [SerializeField] private SpriteRenderer _spriteRenderer;
+        [SerializeField] private Material _material;
         [SerializeField] private float _blendDuration;
 
+        private Material _instanceMaterial;
         private float _blend;
+
+        private void Start()
+        {
+            _instanceMaterial = new Material(_material);
+            _spriteRenderer.material = _instanceMaterial;
+        }
 
         private void LateUpdate()
         {
             _blend += (1f / _blendDuration) * Time.deltaTime;
 
             Color color = Color.Lerp(_initialColor, _targetColor, _blend);
+            _instanceMaterial.SetColor("_TargetColor", color);
 
             _spriteRenderer.color = color;
 
